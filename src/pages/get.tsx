63e6a53cef30ceaaf30da9909f60120e5e3bd436
sadness_ojisan <sadness.ojisan@gmail.com>
2020-03-27 16:50:12 +0900

    chore: add data

--- src/pages/get.tsx
+++ src/pages/get.tsx
@@ -1,5 +1,23 @@
 import * as React from "react";
 
 export default function GetData() {
-  return <div>GetData</div>;
+  const [data, setId] = React.useState<string | null>(null);
+
+  return (
+    <div>
+      Get {data}{" "}
+      <button
+        onClick={() => {
+          fetch(`http://localhost:3000/api/gen?data=${Math.random()}`).then(
+            res =>
+              res.json().then(d => {
+                setId(d.data);
+              })
+          );
+        }}
+      >
+        getId
+      </button>
+    </div>
+  );
 }
